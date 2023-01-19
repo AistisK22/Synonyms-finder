e40e20330f4e5d50766bc293768f12764ebe9ad5
AistisK22 <100942024+AistisK22@users.noreply.github.com>
2023-01-19 23:23:38 +0200

    Styling changes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import './style.css';
-
+import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
+import 'primereact/resources/primereact.min.css'; //core css
+import 'primeicons/primeicons.css';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 export default function App() {
   const [word, setWord] = React.useState('');
   const [syn, setSyn] = React.useState<any[]>([]);
@@ -18,25 +22,35 @@ export default function App() {
 
   return (
     <div>
-      <form>
-        <label htmlFor="word">Your word:</label> <br />
-        <input onChange={(e) => setWord(e.target.value)} placeholder="Word" />
-        <input
-          id="submit"
-          onClick={(e) => find(e)}
-          type="submit"
-          value="Find"
-        />
-      </form>
-      <h3>
-        <b>{word}</b> synonyms:
-      </h3>
-      <ul>
-        {syn.map((it) => (
-          <li key={it.id}>{it.word}</li>
-        ))}
-        {syn.length < 1 && <p>No results</p>}
-      </ul>
+      <h1 style={{ textAlign: 'center', color: 'white' }}>Synonym finder</h1>
+      <div id="main">
+        <form>
+          <label htmlFor="word">Type in a word:</label> <br />
+          <InputText
+            onChange={(e) => setWord(e.target.value)}
+            placeholder="e. g. Awesome"
+          />
+          <Button
+            onClick={(e) => find(e)}
+            icon="pi pi-search"
+            className="p-button-rounded p-button-success p-button-text"
+            aria-label="Search"
+          />
+        </form>
+      </div>
+      {syn.map((it) => (
+        <p
+          id="list"
+          style={{ textAlign: 'center', color: 'white' }}
+          key={it.id}
+        >
+          <i className="pi pi-circle" style={{ fontSize: '0.25em' }}></i>{' '}
+          {it.word}
+        </p>
+      ))}
+      {syn.length < 1 && (
+        <p style={{ textAlign: 'center', color: 'white' }}>No results</p>
+      )}
     </div>
   );
 }
